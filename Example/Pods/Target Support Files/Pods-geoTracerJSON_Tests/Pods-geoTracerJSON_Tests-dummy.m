3244439c66fb3bc641e5040e9b5b14a42aed78a3
Ludovic FAVIER <lfavier@live.fr>
2022-01-07 20:56:42 +0100

    first commit

---
+++ Example/Pods/Target Support Files/Pods-geoTracerJSON_Tests/Pods-geoTracerJSON_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_geoTracerJSON_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_geoTracerJSON_Tests
+@end
---
+++ Example/Pods/Target Support Files/geoTracerJSON/geoTracerJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_geoTracerJSON : NSObject
+@end
+@implementation PodsDummy_geoTracerJSON
+@end
