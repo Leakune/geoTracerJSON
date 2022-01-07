6c79422ae585c1ed5089b6a620648f53a0d3dfbd
Ludovic FAVIER <lfavier@live.fr>
2022-01-07 22:01:31 +0100

    add examples

--- Example/geoTracerJSON/ViewController.swift
+++ Example/geoTracerJSON/ViewController.swift
@@ -6,19 +6,17 @@
 //  Copyright (c) 2022 Leakune. All rights reserved.
 //
 
-import UIKit
+import geoTracerJSON
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainView.backgroundColor = .green
+        mainView.addShadow()
+        mainView.addCornerRadius()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
